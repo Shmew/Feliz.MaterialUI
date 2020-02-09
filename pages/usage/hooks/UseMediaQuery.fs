394c58b2727da22f0f5cc656809045fc67c4d096
Christer van der Meeren <cmeeren@protonmail.com>
2020-02-09 20:21:34 +0100

    Updates

---
+++ pages/usage/hooks/UseMediaQuery.fs
@@ -0,0 +1,21 @@
+﻿module Samples.Usage.Hooks.UseMediaQuery
+
+open Elmish
+open Feliz
+open Feliz.MaterialUI
+
+
+type private Props = { key: string }
+
+
+let private sample = React.functionComponent("UseMediaQuery", (fun props -> props.key), fun _ ->
+  let isDarkMode = Hooks.useMediaQuery "@media (prefers-color-scheme: dark)"
+  [
+    Mui.typography [
+      typography.children ("System dark mode is currently " + if isDarkMode then "enabled" else "disabled")
+    ]
+  ])
+
+
+let getSample (key: string) =
+  sample { key = key }
---
+++ pages/usage/localization/Localization.fs
@@ -0,0 +1,50 @@
+﻿module Samples.Usage.Localization.Localization
+
+open Elmish
+open Feliz
+open Feliz.MaterialUI
+
+
+type private Props = { key: string }
+
+
+let private sample = React.functionComponent("Localization", (fun props -> props.key), fun _ -> [
+  Mui.themeProvider [
+    // createMuiTheme as used below extends the existing theme, which is necessary due to
+    // this documentation's styling solution; there is also a createMuiTheme overload
+    // accepting theme props as the first argument as described in the theming
+    // documentation.
+    themeProvider.theme (fun theme -> Styles.createMuiTheme(theme, Locale.svSE))
+    themeProvider.children [
+      Mui.container [
+        container.maxWidth.xs
+        container.children [
+          Mui.tablePagination [
+            tablePagination.count 20
+            tablePagination.rowsPerPage 10
+            tablePagination.page 1
+            tablePagination.component' "div"
+            tablePagination.onChangePage (fun _ -> ())
+          ]
+          Mui.autocomplete [
+            autocomplete.options [||]
+            prop.style [
+              style.width 300
+            ]
+            autocomplete.renderInput (fun parameters ->
+              Mui.textField [
+                yield! parameters.felizProps
+                textField.variant.outlined
+                textField.fullWidth true
+              ]
+            )
+          ]
+        ]
+      ]
+    ]
+  ]
+])
+
+
+let getSample (key: string) =
+  sample { key = key }
