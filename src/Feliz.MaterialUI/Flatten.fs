2197d90f3656d16b01e6b9ecf7be0a7faa15675c
Ilja Nosik <ilja.nosik@outlook.com>
2020-11-04 17:07:29 +0100

    Wrap fromFlatEntries JS code into an IIFE
    This works around a regression in Fable 3, see fable-compiler/Fable#2249.

--- src/Feliz.MaterialUI/Flatten.fs
+++ src/Feliz.MaterialUI/Flatten.fs
@@ -8,25 +8,27 @@ open Fable.Core
 module Object =
 
   [<Emit("""
-function setProperty(target, key, value) {
-  const sepIdx = key.indexOf('.')
-  if (sepIdx === -1) {
-    target[key] = value
-  } else {
-    const topKey = key.substring(0, sepIdx)
-    const nestedKey = key.substring(sepIdx + 1)
-    if (target[topKey] === undefined) {
-      target[topKey] = {}
+(function () {
+  function setProperty(target, key, value) {
+    const sepIdx = key.indexOf('.')
+    if (sepIdx === -1) {
+      target[key] = value
+    } else {
+      const topKey = key.substring(0, sepIdx)
+      const nestedKey = key.substring(sepIdx + 1)
+      if (target[topKey] === undefined) {
+        target[topKey] = {}
+      }
+      setProperty(target[topKey], nestedKey, value)
     }
-    setProperty(target[topKey], nestedKey, value)
   }
-}
 
-const target = {}
-for (let kv of $0) {
-  setProperty(target, kv[0], kv[1])
-}
+  const target = {}
+  for (let kv of $0) {
+    setProperty(target, kv[0], kv[1])
+  }
 
-return target
+  return target
+})()
 """)>]
   let fromFlatEntries (kvs: seq<string * obj>) : obj = jsNative
